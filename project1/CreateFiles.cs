89e9151de7aa44eb52057447ecccbe6b53e31ee0
Aaron Little <ventureaaron@hotmail.com>
2016-10-04 00:29:11 -0500

    Project 1 File Generation
    These files (CreateFiles/ExtensionMethods) are used to create the random
    arrays and then serialize them to files for storage

---
+++ project1/CreateFiles.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace Project1_createFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRandFiles = 10;
+            int numberOfOrderedFiles = 1;
+            int numberOfRevOrderedFiles = 1;
+
+            int[] fiftyThouArray = new int[50000];
+            int[] twoHundredThouArray = new int[200000];
+            int[] fiveHundredThouArray = new int[500000];
+            int[] oneMillionArray = new int[1000000];
+
+            string baseOutPath = "C:\\Users\\aaron\\Desktop\\Serial_Array_Files\\";
+
+            for (int i = 0; i < numberOfRandFiles; i++)
+            {
+                fiftyThouArray.RandomGenArray();
+                fiftyThouArray.WriteSerial(baseOutPath + "Rand_50_thou_" + (i+1)+".ser");
+
+                twoHundredThouArray.RandomGenArray();
+                twoHundredThouArray.WriteSerial(baseOutPath + "Rand_200_thou_" + (i+1)+".ser");
+
+                fiveHundredThouArray.RandomGenArray();
+                fiveHundredThouArray.WriteSerial(baseOutPath + "Rand_500_thou_" + (i+1)+".ser");
+
+                oneMillionArray.RandomGenArray();
+                oneMillionArray.WriteSerial(baseOutPath + "Rand_million_" + (i + 1) + ".ser");
+            }
+
+            for (int i = 0; i < numberOfOrderedFiles; i++)
+            {
+                fiftyThouArray.OrderGenArray();
+                fiftyThouArray.WriteSerial(baseOutPath + "Ordered_50_thou_" + (i + 1) + ".ser");
+
+                twoHundredThouArray.OrderGenArray();
+                twoHundredThouArray.WriteSerial(baseOutPath + "Ordered_200_thou_" + (i + 1) + ".ser");
+
+                fiveHundredThouArray.OrderGenArray();
+                fiveHundredThouArray.WriteSerial(baseOutPath + "Ordered_500_thou_" + (i + 1) + ".ser");
+
+                oneMillionArray.OrderGenArray();
+                oneMillionArray.WriteSerial(baseOutPath + "Ordered_million_" + (i + 1) + ".ser");
+            }
+
+            for (int i = 0; i < numberOfRevOrderedFiles; i++)
+            {
+                fiftyThouArray.ReverseOrderGenArray();
+                fiftyThouArray.WriteSerial(baseOutPath + "Rev_Ordered_50_thou_" + (i + 1) + ".ser");
+
+                twoHundredThouArray.ReverseOrderGenArray();
+                twoHundredThouArray.WriteSerial(baseOutPath + "Rev_Ordered_200_thou_" + (i + 1) + ".ser");
+
+                fiveHundredThouArray.ReverseOrderGenArray();
+                fiveHundredThouArray.WriteSerial(baseOutPath + "Rev_Ordered_500_thou_" + (i + 1) + ".ser");
+
+                oneMillionArray.ReverseOrderGenArray();
+                oneMillionArray.WriteSerial(baseOutPath + "Rev_Ordered_million_" + (i + 1) + ".ser");
+            }
+        }
+        
+    }
+}
---
+++ project1/ExtensionMethods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static void Populate<T>(this T[] arr, T value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = value;
+            }
+        }
+        public static void WriteSerial<T>(this T[] arr, String outpath)
+        {
+            try
+            {
+                using (Stream stream = File.Open(outpath, FileMode.Create))
+                {
+                    BinaryFormatter bin = new BinaryFormatter();
+                    bin.Serialize(stream, arr);
+                }
+            }
+            catch (IOException)
+            {
+
+            }
+        }
+        public static void RandomGenArray(this int[] arr)
+        {
+            Random rnd = new Random();
+            arr.Populate(0);
+            for (int i = 0; i< arr.Length; i++)
+            {
+                while (true)
+                {
+                    int x = rnd.Next(0, arr.Length);
+                    if (arr[x] == 0)
+                    {
+                        arr[x] = i + 1;
+                        break;
+                    }
+                    else continue;
+                }
+            }
+        }
+        public static void OrderGenArray(this int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]= i + 1;
+            }
+        }
+        public static void ReverseOrderGenArray(this int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr.Length - i;
+            }
+        }
+    }
+}
